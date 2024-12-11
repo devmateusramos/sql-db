64c7a7c84b29935312586e2299976341f7ba92b8
mateusramos <devmateusramos@gmail.com>
2024-12-11 09:48:22 -0300

    btree type

--- golang/btree/btree.go
+++ golang/btree/btree.go
@@ -19,3 +19,22 @@ func init() {
 }
 
 type BNode []byte // can be dumped to the disk
+
+/*
+ B+Tree the database file is an array of pages(nodes) referenced
+by page numbers (pointers)
+
+* get reads a page from disk
+* new allocates and writes a new page (copy-on-write)
+* del deallocates a page
+
+*/
+
+type BTree struct {
+	// pointer (a nonzero page number)
+	root uint64
+	// callbacks fro manging on-disk pages
+	get func(uint64) []byte // dereference a pointer
+	new func([]byte) uint64 // allocate a new page
+	del func(uint64)        // deallocate a page
+}
